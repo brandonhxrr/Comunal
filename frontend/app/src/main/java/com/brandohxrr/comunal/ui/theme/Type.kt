2464ee3ffb08c2db0970ec3fc0c0ddd6a6816490
Brandon Herrera <brandondh769@gmail.com>
2024-04-13 13:52:43 -0600

    Add fonts and colors

--- frontend/app/src/main/java/com/brandohxrr/comunal/ui/theme/Type.kt
+++ frontend/app/src/main/java/com/brandohxrr/comunal/ui/theme/Type.kt
@@ -2,33 +2,39 @@ package com.brandohxrr.comunal.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.brandohxrr.comunal.R
 
-// Set of Material typography styles to start with
 val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
+    bodySmall = TextStyle(
+        fontFamily = FontFamily(Font(R.font.space_grotesk_medium)),
+        fontSize = 14.sp
     ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
-    )
-    */
+    bodyMedium = TextStyle(
+        fontFamily = FontFamily(Font(R.font.space_grotesk_medium)),
+        fontSize = 16.sp
+    ),
+    headlineMedium = TextStyle(
+        fontFamily = FontFamily(Font(R.font.clash_display)),
+        fontWeight = FontWeight.Bold,
+        fontSize = 18.sp
+    ),
+    titleSmall = TextStyle(
+        fontFamily = FontFamily(Font(R.font.inter_medium)),
+        fontWeight = FontWeight.Bold,
+        fontSize = 20.sp
+    ),
+    titleMedium = TextStyle(
+        fontFamily = FontFamily(Font(R.font.clash_display)),
+        fontSize = 24.sp,
+        fontWeight = FontWeight.Bold
+    ),
+    titleLarge = TextStyle(
+        fontFamily = FontFamily(Font(R.font.space_grotesk_bold)),
+        fontWeight = FontWeight.Bold,
+        fontSize = 40.sp
+    ),
 )
